2ec74e8e16bb86cc752f3f2ea4b4fbf8536fa015
Abhijith Reddy <areddy@voxware.com>
2013-07-24 22:35:43 -0400

    Added maps and google places library

--- src/com/archerabi/wheredoigo/DirectionPointerCanvas.java
+++ src/com/archerabi/wheredoigo/DirectionPointerCanvas.java
@@ -4,6 +4,8 @@
 package com.archerabi.wheredoigo;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Matrix;
@@ -13,53 +15,65 @@ import android.view.View;
 
 /**
  * @author gautamichitteti
- *
+ * 
  */
 public class DirectionPointerCanvas extends View {
 
 	private Paint paint = new Paint();
-	
+
 	private float angle = 0;
-	
-	private int radius ;
-	
+
+	private int radius;
+
 	private Matrix originalMatrix = null;
+
+	private Bitmap arrowBitMap = null;
 	
 	public DirectionPointerCanvas(Context context) {
 		super(context);
+		
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see android.view.View#onDraw(android.graphics.Canvas)
 	 */
 	@Override
 	protected void onDraw(Canvas canvas) {
-		
-			if(originalMatrix == null){
-				originalMatrix = canvas.getMatrix();
-			}
-			radius = getWidth() / 3;
-			int centerx = getWidth()/2;
-			int centery = getHeight()/2;
-	        paint.setStyle(Paint.Style.FILL);
-	        canvas.drawColor(Color.WHITE);
-	        paint.setColor(Color.BLUE);
-	        canvas.setMatrix(originalMatrix);
-	        //Translate to the center of the canvas, rotate by angle and retranslate back to original origin
-	        canvas.translate(centerx, centery);
-	        canvas.rotate(angle);
-	        canvas.translate(-centerx, -centery);
-	        
-	        int x = (int)(centerx);
-	        int y = (int)(centery - radius);
-	        canvas.drawText("N", x, y, paint);
+
+		if (originalMatrix == null) {
+			originalMatrix = canvas.getMatrix();
+		}
+		if(arrowBitMap == null){
+			arrowBitMap = BitmapFactory.decodeResource(getResources(),R.drawable.arrow);
+			arrowBitMap = Bitmap.createScaledBitmap(arrowBitMap,(int)(getWidth()*0.6), (int)(getWidth() * 0.6), false);
+		}
+		radius = getWidth() / 3;
+		int centerx = getWidth() / 2;
+		int centery = getHeight() / 2;
+		paint.setStyle(Paint.Style.FILL);
+		canvas.drawColor(Color.WHITE);
+		paint.setColor(Color.BLUE);
+		canvas.setMatrix(originalMatrix);
+		// Translate to the center of the canvas, rotate by angle and
+		// retranslate back to original origin
+		canvas.translate(centerx, centery);
+		canvas.rotate(angle);
+		canvas.translate(-centerx, -centery);
+
+		int x = (int) (centerx);
+		int y = (int) (centery - radius);
+		canvas.drawText("N", x, y, paint);
+
+		canvas.drawBitmap(arrowBitMap, centerx - arrowBitMap.getWidth()/2, centery - arrowBitMap.getHeight()/2, null);
 	}
 
 	/**
-	 * @param angle in degrees
+	 * @param angle
+	 *            in degrees
 	 */
-	public void setAngle(int angle){
-		Log.d(getClass().getName(),"Angle set to "+angle);
+	public void setAngle(int angle) {
 		this.angle = angle;
 		invalidate();
 	}
--- src/com/archerabi/wheredoigo/MainActivity.java
+++ src/com/archerabi/wheredoigo/MainActivity.java
@@ -1,7 +1,16 @@
 package com.archerabi.wheredoigo;
 
-import android.app.Activity;
+import java.io.IOException;
+import java.util.List;
+
+import org.apache.http.client.ClientProtocolException;
+import org.gmarz.googleplaces.GooglePlaces;
+import org.gmarz.googleplaces.models.Place;
+import org.gmarz.googleplaces.models.PlacesResult;
+import org.json.JSONException;
+
 import android.content.Context;
+import android.content.pm.ActivityInfo;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
@@ -10,37 +19,56 @@ import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.util.Log;
 import android.view.Menu;
-import android.widget.LinearLayout;
 
-public class MainActivity extends Activity implements SensorEventListener,LocationListener{
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.CameraPositionCreator;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends FragmentActivity implements SensorEventListener, LocationListener {
 
 	private SensorManager mSensorManager;
 	private Sensor mMagneticSensor;
 	private Sensor mAccelSensor;
+
 	private LocationManager mLocationManager;
-	
+
 	private float[] latestAccelValues = null;
 	private float[] latestMagValues = null;
-	
+
 	private long lastTimeStamp;
-	
-	private DirectionPointerCanvas directionCanvas ;
+
+	private GooglePlaces gPlaces;
+
+	private DirectionPointerCanvas directionCanvas;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
-		LinearLayout layout = (LinearLayout) findViewById(R.id.root_layout);
-		directionCanvas = new DirectionPointerCanvas(getApplicationContext());
-		layout.addView(directionCanvas);
-		
+
+		// LinearLayout layout = (LinearLayout) findViewById(R.id.root_layout);
+		// directionCanvas = new
+		// DirectionPointerCanvas(getApplicationContext());
+		// layout.addView(directionCanvas);
+
 		mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
 		mMagneticSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
 		mAccelSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
 		mLocationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
 		mLocationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 10000, 10, this);
+
+		setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+		
 	}
 
 	@Override
@@ -52,10 +80,12 @@ public class MainActivity extends Activity implements SensorEventListener,Locati
 
 	@Override
 	public void onAccuracyChanged(Sensor sensor, int accuracy) {
-		
+
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see android.app.Activity#onPause()
 	 */
 	@Override
@@ -65,56 +95,99 @@ public class MainActivity extends Activity implements SensorEventListener,Locati
 		mLocationManager.removeUpdates(this);
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see android.app.Activity#onResume()
 	 */
 	@Override
 	protected void onResume() {
 		super.onResume();
-		mSensorManager.registerListener(this, mMagneticSensor, SensorManager.SENSOR_DELAY_UI);
-		mSensorManager.registerListener(this, mAccelSensor,SensorManager.SENSOR_DELAY_UI);
-		lastTimeStamp = System.currentTimeMillis();
+		// mSensorManager.registerListener(this, mMagneticSensor,
+		// SensorManager.SENSOR_DELAY_UI);
+		// mSensorManager.registerListener(this,
+		// mAccelSensor,SensorManager.SENSOR_DELAY_UI);
+		// lastTimeStamp = System.currentTimeMillis();
+		new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+				gPlaces = new GooglePlaces("AIzaSyC1kNWXkXU4N68Cd05eUlGomKamHZ4he6A");
+				try {
+					PlacesResult result = gPlaces.getPlaces("coffee", 200, 40.288414, -74.548978);
+					final List<Place> places = result.getPlaces();
+					runOnUiThread(new Runnable() {
+						
+						@Override
+						public void run() {
+							for (Place place : places) {
+								Log.d(getClass().getName(), place.getName());
+								LatLng latLng = new LatLng(place.getLatitude(), place.getLongitude());
+								GoogleMap mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map)).getMap();
+								CameraPosition position = CameraPosition.fromLatLngZoom(latLng, 15);
+								mMap.animateCamera(CameraUpdateFactory.newCameraPosition(position));
+								mMap.addMarker(new MarkerOptions().position(latLng).icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED))).setSnippet(place.getName());
+							}
+						}
+					});
+				} catch (ClientProtocolException e) {
+					e.printStackTrace();
+				} catch (JSONException e) {
+					e.printStackTrace();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}).start();
 	}
 
 	@Override
 	public void onSensorChanged(SensorEvent event) {
-		if(event.sensor == mAccelSensor){
+		if (event.sensor == mAccelSensor) {
 			latestAccelValues = event.values.clone();
-		}else if(event.sensor == mMagneticSensor){
+		} else if (event.sensor == mMagneticSensor) {
 			latestMagValues = event.values.clone();
 		}
-		if( latestMagValues != null && latestAccelValues != null){
-			float[] rotationMatrix = new float[16];
-			SensorManager.getRotationMatrix(rotationMatrix, null, latestAccelValues, latestMagValues);
+		if (latestMagValues != null && latestAccelValues != null) {
+			float[] rotationMatrix = new float[9];
+			float[] outR = new float[9];
 			float[] orientation = new float[3];
-			SensorManager.getOrientation(rotationMatrix, orientation);
-			if(System.currentTimeMillis() - lastTimeStamp > 30){
-//				Log.d(getClass().getName(),"Rotation around z = "+String.valueOf(orientation[0] * 180/Math.PI));
+			SensorManager.getRotationMatrix(rotationMatrix, null, latestAccelValues, latestMagValues);
+			SensorManager.remapCoordinateSystem(rotationMatrix, SensorManager.AXIS_X, SensorManager.AXIS_Z, outR);
+			SensorManager.getOrientation(outR, orientation);
+			if (System.currentTimeMillis() - lastTimeStamp > 150) {
+				// Log.d(getClass().getName(),"Rotation around z = "+String.valueOf(Math.toDegrees(orientation[0]))+"\tRotation around y = "+String.valueOf(Math.toDegrees(orientation[1])+"\tRotation around x = "+String.valueOf(Math.toDegrees(orientation[2]))));
 				lastTimeStamp = System.currentTimeMillis();
-				directionCanvas.setAngle((int) (- orientation[0] * 180 / Math.PI));
+				directionCanvas.setAngle((int) (-1 * Math.toDegrees(orientation[0])));
 			}
 		}
+
 	}
 
 	@Override
 	public void onLocationChanged(Location location) {
-		Log.d(getClass().getName(),"Lat = "+String.valueOf(location.getLatitude())+" Long = "+String.valueOf(location.getLongitude()));
+		Log.d(getClass().getName(), "Lat = " + String.valueOf(location.getLatitude()) + " Long = " + String.valueOf(location.getLongitude()));
+		LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+		GoogleMap mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map)).getMap();
+		CameraPosition position = CameraPosition.fromLatLngZoom(latLng, 15);
+		mMap.animateCamera(CameraUpdateFactory.newCameraPosition(position));
+		mMap.addMarker(new MarkerOptions().position(latLng).icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE)));
+
 	}
 
 	@Override
 	public void onProviderDisabled(String provider) {
-		Log.d(getClass().getName(),provider + " is DISABLED");
+		Log.d(getClass().getName(), provider + " is DISABLED");
 	}
 
 	@Override
 	public void onProviderEnabled(String provider) {
-		Log.d(getClass().getName(),provider + " is ENABLED");
+		Log.d(getClass().getName(), provider + " is ENABLED");
 	}
 
 	@Override
 	public void onStatusChanged(String provider, int status, Bundle extras) {
-		// TODO Auto-generated method stub
-		
-	} 
+
+	}
 
 }
